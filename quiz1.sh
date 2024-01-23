a241e177525e133644b09ca5288ce4b42bcad39a
alexandriaskinner <alexandriaskinner25@gmail.com>
2024-01-23 09:27:04 -0800

    update

--- quiz1.sh
+++ quiz1.sh
@@ -1,3 +1,4 @@
+# Authors: Christina Chen, Alexandria Skinner
 echo "Alexandria Skinner
 User: $USER"
 
@@ -8,3 +9,5 @@ gunzip -c dictionary | grep -E "^[tabilrn]*b[tabilrn]*" | grep -E "...." | wc
 gunzip -c dictionary | grep -E "^[maocdin]*c[maocdin]*" | grep -E "...." | wc
 
 gunzip -c dictionary | grep -E "^[anozigr]*z[anozigr]*" | grep -E "...." | wc
+
+gunzip -c jaspar2024_core.transfac.gz | grep -E "tax_group"| sort -n | uniq -c
