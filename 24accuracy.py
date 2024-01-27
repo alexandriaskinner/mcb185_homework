a2d150e5c314daf4aab928b133f61b598a90d596
alexandriaskinner <alexandriaskinner25@gmail.com>
2024-01-27 10:04:56 -0800

    update

--- 24accuracy.py
+++ 24accuracy.py
@@ -2,7 +2,7 @@
 
 def accuracy_F1(TP, TN, FP, FN):
 	A  = (TP+TN) / (TP+TN+FP+FN)	#Accuracy
-	p  = TP / (TP+FP)					#precision - to calculate F1
+	p  = TP / (TP+FP)				#precision - to calculate F1
 	r  = TP / (TP+FN)				#recall - to calculate F1
 	F1 = 2*p*r / (p+r)				#F1
 	return A, F1
